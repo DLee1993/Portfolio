de75aff08d1bd31118397c2ae6236da44791d419
David Lee <Dai180293@gmail.com>
2024-07-17 22:41:48 +0100

    changed text to link to root page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,13 @@
 import logo from "../../public/faviconPNG.png";
 import Image from "next/image";
+import Link from "next/link";
 function Header() {
     return (
-        <header className="flex justify-start items-center py-5 sm:py-10 gap-x-4 px-5 sm:px-10">
-            <Image src={logo} alt="personal logo" className="rounded-full size-6" />
-            <h1 className="uppercase font-inter text-small">
-                developer / kickboxer
-            </h1>
+        <header className="py-5 sm:py-10 px-5 sm:px-10">
+            <Link href="/" className="flex justify-start items-center gap-x-4">
+                <Image src={logo} alt="personal logo" className="rounded-full size-6" />
+                <h1 className="uppercase font-inter text-small">developer / kickboxer</h1>
+            </Link>
         </header>
     );
 }
