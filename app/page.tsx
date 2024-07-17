face65f63943f65264878768d27562198bd4801e
David Lee <Dai180293@gmail.com>
2024-07-17 22:43:18 +0100

    changed the flex layout to items center

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Showcase from "./components/Showcase";
 export default function page() {
     return (
         <section>
-            <section className="flex justify-start items-end sm:items-center text-small sm:text-base py-5 px-5 md:pl-40 lg:pl-52 min-h-60">
+            <section className="flex justify-start items-center text-small sm:text-base py-5 px-5 md:pl-40 lg:pl-52 min-h-60">
                 <article>
                     <p className="mb-5">
                         Hi I&apos;m David, a Frontend Developer based in{" "}
