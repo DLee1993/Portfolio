1044902b72083f57383ef62807f411386b276108
David Lee <Dai180293@gmail.com>
2024-07-17 14:51:07 +0100

    removed media query for header text

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,9 +2,11 @@ import logo from "../../public/faviconPNG.png";
 import Image from "next/image";
 function Header() {
     return (
-        <header className="flex justify-start items-center py-10 gap-x-4">
-            <Image src={logo} alt="personal logo" className="hidden sm:block" />
-            <h1 className="uppercase font-inter">developer / kickboxer</h1>
+        <header className="flex justify-start items-center py-5 sm:py-10 gap-x-4 px-5 sm:px-10">
+            <Image src={logo} alt="personal logo" className="rounded-full size-6" />
+            <h1 className="uppercase font-inter text-small">
+                developer / kickboxer
+            </h1>
         </header>
     );
 }
