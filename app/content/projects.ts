3dd34505d1d069c76890b83650be3b34a45a6f21
David Lee <Dai180293@gmail.com>
2024-07-23 23:15:03 +0100

    added template content for all showcase projects

--- app/content/projects.ts
+++ app/content/projects.ts
@@ -1,15 +1,41 @@
+import { ProjectObject } from "@/types";
+import scribblepad from "../../public/scribblePad_j0miez_c_scale,w_1024.webp";
+
 const projects: ProjectObject[] = [
     {
-        title: "first title",
-        content: "content",
+        title: "Scribblepad",
+        content: "Your one stop shop for all your organisational needs.",
+        tags: ["Next.js", "React"],
+        href: "https://www.google.com",
+        image: scribblepad,
     },
     {
-        title: "second title",
-        content: "content",
+        title: "Scribblepad",
+        content: "Your one stop shop for all your organisational needs.",
+        tags: ["Next.js", "React"],
+        href: "https://www.google.com",
+        image: scribblepad,
     },
     {
-        title: "third and last title",
-        content: "content",
+        title: "Scribblepad",
+        content: "Your one stop shop for all your organisational needs.",
+        tags: ["Next.js", "React"],
+        href: "https://www.google.com",
+        image: scribblepad,
+    },
+    {
+        title: "Scribblepad",
+        content: "Your one stop shop for all your organisational needs.",
+        tags: ["Next.js", "React"],
+        href: "https://www.google.com",
+        image: scribblepad,
+    },
+    {
+        title: "Scribblepad",
+        content: "Your one stop shop for all your organisational needs.",
+        tags: ["Next.js", "React"],
+        href: "https://www.google.com",
+        image: scribblepad,
     },
 ];
 
--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,9 @@
+import { StaticImageData } from "next/image";
+
 type ProjectObject = {
     title: string;
     content: string;
+    tags: string[];
+    href: string;
+    image: StaticImageData,
 };
