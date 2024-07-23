73a030a39bfc3cb5d98e087b1ff98510008a2059
David Lee <Dai180293@gmail.com>
2024-07-23 23:17:28 +0100

    updated styling to header content

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,25 +3,29 @@ import { ImArrowUpRight2 } from "react-icons/im";
 
 export default function Header() {
     return (
-        <header className="min-h-28 w-full sticky top-0 z-50 flex justify-between items-center">
+        <header className="min-h-28 w-full sticky top-0 z-50 flex justify-between items-center customPadding bg-gradient-to-b from-csBlack via-csBlack/50 to-transparent">
             <div>
                 <p className="text-xl font-semibold leading-none">David Lee</p>
-                <p className="text-xs text-csFadedWhite leading-none">Frontend Developer</p>
+                <p className="text-xs text-csFadedWhite">Frontend Developer</p>
             </div>
             <ul className="flex justify-center items-center gap-x-5 capitalize text-sm">
-                <li className="flex justify-center items-center gap-x-1 group">
-                    <Link href="#">linkedin</Link>
-                    <ImArrowUpRight2
-                        size="0.75rem"
-                        className="group-hover:-translate-y-[2px] group-hover:translate-x-[2px] transition-all"
-                    />
+                <li className="group">
+                    <Link href="#" className="flex justify-center items-center gap-x-1">
+                        linkedin{" "}
+                        <ImArrowUpRight2
+                            size="0.75rem"
+                            className="group-hover:-translate-y-[2px] group-hover:translate-x-[2px] transition-all"
+                        />
+                    </Link>
                 </li>
-                <li className="flex justify-center items-center gap-x-1 group">
-                    <Link href="#">github</Link>
-                    <ImArrowUpRight2
-                        size="0.75rem"
-                        className="group-hover:-translate-y-[2px] group-hover:translate-x-[2px] transition-all"
-                    />
+                <li className="group">
+                    <Link href="#" className="flex justify-center items-center gap-x-1">
+                        github{" "}
+                        <ImArrowUpRight2
+                            size="0.75rem"
+                            className="group-hover:-translate-y-[2px] group-hover:translate-x-[2px] transition-all"
+                        />
+                    </Link>
                 </li>
             </ul>
         </header>
