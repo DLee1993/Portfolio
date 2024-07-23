2f9a40b13e9358221bf4ec7ca4215f47a2f839a9
David Lee <Dai180293@gmail.com>
2024-07-23 23:16:53 +0100

    added showcase component to root

--- app/page.tsx
+++ app/page.tsx
@@ -2,29 +2,35 @@
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import Showcase from "./components/Showcase";
 gsap.registerPlugin(useGSAP);
 
 export default function Page() {
     const container = useRef(null);
 
-    // useGSAP(
-    //     () => {
-    //         gsap.to("span", { opacity: 1 });
-    //     },
-    //     { scope: container }
-    // );
+    useGSAP(
+        () => {
+            gsap.to("section", { opacity: 1, y: 0, stagger: 0.025 });
+        },
+        { scope: container }
+    );
 
     return (
-        <section ref={container} className="flex justify-center items-center min-h-[45vh]">
-            <h1 className="max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl flex flex-col justify-center items-center md:items-end gap-y-4">
-                <span className="tracking-tighter text-lg sm:text-xl md:text-2xl lg:text-3xl text-center">
-                    I build accessible, modern web apps to help streamline the needs of people and
-                    businesses.
-                </span>
-                <span className="text-csFadedWhite text-xs">
-                    Frontend Developer base in South Wales, UK.
-                </span>
-            </h1>
+        <section ref={container} className="flex flex-col items-center gap-y-20">
+            <section className="flex justify-center items-center min-h-[45vh] opacity-0 translate-y-10">
+                <h1 className="max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl flex flex-col justify-center items-center md:items-end gap-y-4">
+                    <span className="tracking-tighter text-lg sm:text-xl md:text-2xl lg:text-3xl text-center">
+                        I build accessible, modern web apps to help streamline the needs of people
+                        and businesses.
+                    </span>
+                    <span className="text-csFadedWhite text-xs">
+                        Frontend Developer based in South Wales, UK.
+                    </span>
+                </h1>
+            </section>
+            <section className="opacity-0 translate-y-10 w-full">
+                <Showcase />
+            </section>
         </section>
     );
 }
