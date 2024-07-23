4a51a5db6b664c3fc5b6e044cb448f88a622ecd0
David Lee <Dai180293@gmail.com>
2024-07-23 23:15:24 +0100

    added accent color for each showcase project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ const config: Config = {
                 csBlack: "#101010",
                 csWhite: "#F2F2F2",
                 csFadedWhite: "rgba(242,242,242, 0.5)",
+                csPurple: "#432B89",
+                csGreen: "#254441",
+                csBlue: "#102542",
+                csRed: "#621B00",
+                csOrange: "#FF6B00",
             },
             fontFamily: {
                 inter: "var(--font-inter)",
