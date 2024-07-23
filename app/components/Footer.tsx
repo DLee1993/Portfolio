e847fd64c5561ad693ca22f69e8806069f6adb5f
David Lee <Dai180293@gmail.com>
2024-07-23 23:20:59 +0100

    added footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="min-h-[50vh] customPadding flex justify-center items-center border-t-2 border-csFadedWhite/10 mt-20">
+        footer content
+    </footer>
+  )
+}
