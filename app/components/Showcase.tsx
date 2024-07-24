3e9b1591ad91e11ba8f8e968b1a452b8d9e1cab6
David Lee <Dai180293@gmail.com>
2024-07-24 04:34:00 +0100

    adjusted the color opacity for each project, added keyboard focus styling for each project

--- app/components/Showcase.tsx
+++ app/components/Showcase.tsx
@@ -13,6 +13,7 @@ export default function Showcase() {
                         target="_blank"
                         id="project"
                         className="relative block p-2 rounded-xl md:rounded-3xl bg-csWhite/5 overflow-hidden group"
+                        aria-label={`This is a link to the personal project ${project.title}`}
                     >
                         <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-1 bg-gradient-radial from-csWhite/60 via-csBlack/10 to-transparent"></div>
                         <section
@@ -21,15 +22,15 @@ export default function Showcase() {
                             <div
                                 className={`absolute top-0 left-0 w-full h-full bg-gradient-to-b ${
                                     index === 0
-                                        ? "from-csGreen/50"
+                                        ? "from-csGreen/75"
                                         : index === 1
-                                        ? "from-csPurple/30"
+                                        ? "from-csPurple/50"
                                         : index === 2
-                                        ? "from-csBlue/50"
+                                        ? "from-csBlue/75"
                                         : index === 3
-                                        ? "from-csRed/50"
-                                        : "from-csOrange/30"
-                                } to-csBlack/10 md:border-[1px] border-csWhite rounded-xl md:rounded-3xl md:opacity-0 group-hover:opacity-100 transition-all duration-500`}
+                                        ? "from-csRed/30"
+                                        : "from-csOrange/50"
+                                } to-csBlack/10 md:border-[1px] border-csWhite rounded-xl md:rounded-3xl md:opacity-0 group-hover:opacity-100 group-focus-visible:opacity-100 transition-all duration-500`}
                             ></div>
                             <section className="relative z-50 flex justify-between items-start mb-10 px-2 sm:px-5 md:px-10">
                                 <article>
@@ -55,7 +56,7 @@ export default function Showcase() {
                                     alt="project image"
                                     fill={true}
                                     objectFit="cover"
-                                    className="rounded-t-lg md:rounded-t-xl"
+                                    className="rounded-t-lg md:rounded-t-xl group-hover:scale-105 group-focus-visible:scale-105 transition-all duration-500"
                                 />
                             </figure>
                         </section>
