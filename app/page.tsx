fbc0be2963f79028ecad10c740574ad725d1f987
David Lee <Dai180293@gmail.com>
2024-07-25 12:16:20 +0100

    increased sizing of h1

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,8 @@ export default function Page() {
     return (
         <section ref={container} className="flex flex-col items-center gap-y-20">
             <section className="flex justify-center items-center min-h-[45vh] opacity-0 translate-y-10">
-                <h1 className="max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl flex flex-col justify-center items-center md:items-end gap-y-4">
-                    <span className="tracking-tighter text-lg sm:text-xl md:text-2xl lg:text-3xl text-center">
+                <h1 className="max-w-md md:max-w-lg lg:max-w-2xl flex flex-col justify-center items-center md:items-end gap-y-4">
+                    <span className="tracking-tighter text-xl md:text-2xl lg:text-3xl text-center">
                         I build accessible, modern web apps to help streamline the needs of people
                         and businesses.
                     </span>
