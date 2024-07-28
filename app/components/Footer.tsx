8db363957041a3ab91eca4709d319df75969a2e4
David Lee <Dai180293@gmail.com>
2024-07-28 19:24:45 +0100

    added a ternary to check the pathname and render different content in the footer based on said pathanme

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ImArrowUpRight2 } from "react-icons/im";
-import ContactForm from "./ContactForm";
 
 export default function Footer() {
+    const pathname = usePathname();
+
     const socials = [
         {
             href: "https://www.google.com",
@@ -20,13 +24,40 @@ export default function Footer() {
     return (
         <footer className="min-h-[50vh] customPadding text-sm pt-10 pb-24 md:py-20 border-t-2 border-csFadedWhite/10 mt-20">
             <section className="w-full flex flex-col gap-y-10 md:flex-row justify-around items-start">
-                <article className="max-w-md">
-                    <h2 className="text-lg font-semibold">Thanks for checking out my portfolio</h2>
-                    <p className="text-csFadedWhite mt-2">
-                        If you would like to discuess my portfolio further or discuss potential
-                        employment, please click the button below to get in touch
-                    </p>
-                    <ContactForm />
+                <article className="max-w-md flex flex-col">
+                    {pathname !== "/contact" ? (
+                        <>
+                            <h2 className="text-lg font-semibold">
+                                Thanks for checking out my portfolio
+                            </h2>
+                            <p className="text-csFadedWhite mt-2 mb-7">
+                                If you would like to discuss my portfolio further or discuss
+                                potential employment, please click the button below to get in touch
+                            </p>
+                            <Link
+                                href="/contact"
+                                className="py-2 px-4 w-fit bg-white text-csBlack rounded-full border-2 border-csWhite hover:bg-csBlack hover:text-csWhite transition-colors"
+                            >
+                                Get in touch
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <h2 className="text-lg font-semibold">
+                                Thanks for checking out my portfolio
+                            </h2>
+                            <p className="text-csFadedWhite mt-2 mb-7">
+                                If you would like to download my resume for your personal records,
+                                please click the link below, I look forward to hearing from you.
+                            </p>
+                            <Link
+                                href="/contact"
+                                className="py-2 px-4 w-fit bg-white text-csBlack rounded-full border-2 border-csWhite hover:bg-csBlack hover:text-csWhite transition-colors"
+                            >
+                                Download Resume
+                            </Link>
+                        </>
+                    )}
                 </article>
                 <ul className="capitalize flex flex-col gap-y-2">
                     <li className="text-csFadedWhite mb-3">Digital connections</li>
