dcfff7c241d43d49883f76852f9e00527f3763f2
David Lee <Dai180293@gmail.com>
2024-07-28 19:25:10 +0100

    added schemas and types to contact form for validation

---
+++ schema/FormData.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const FormDataSchema = z.object({
+    user_name: z.string().min(1, "Name is required"),
+    user_email: z.string().min(1, "Email is required").email("Invalid email address"),
+    // user_company: z.string().min(1, "Company name is required"),
+    // message: z.string().min(1, "Message is required"),
+});
+
+export type Inputs = z.infer<typeof FormDataSchema>;
