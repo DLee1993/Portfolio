046441b2e2cd2344540f94908e2201b2514d784e
David Lee <Dai180293@gmail.com>
2024-07-29 17:44:51 +0100

    added all validation and content

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -1,11 +1,141 @@
 "use client";
 
+import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
+import { Inputs, FormDataSchema } from "@/schema/FormData";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 
 export default function ContactForm() {
+    const [formSuccess, setFormSuccess] = useState<boolean>(false);
+    const [formError, setFormError] = useState({ bool: false, message: "" });
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, dirtyFields },
+    } = useForm<Inputs>({
+        mode: "onSubmit",
+        resolver: zodResolver(FormDataSchema),
+        defaultValues: {
+            user_name: "",
+            user_email: "",
+            user_company: "",
+            message: "",
+        },
+    });
+
+    const processForm = () => {
+        const canSave = [dirtyFields].every(Boolean);
+
+        if (!canSave) return;
+
+        emailjs
+            .sendForm("service_tcs5k1e", "contact_form", "#form", {
+                publicKey: "TfFxJ4tLfWO6qwoQi",
+                limitRate: { throttle: 60000 },
+            })
+            .then(
+                () => {
+                    // console.log("SUCCESS!");
+                    setFormSuccess(true);
+                },
+                (error) => {
+                    // console.log("FAILED...", error.text);
+                    setFormError({ bool: true, message: error.text });
+                }
+            );
+    };
+
     return (
-        <section id="contactForm">
-            <section>form goes here</section>
+        <section>
+            <article className="max-w-lg mx-auto text-center">
+                <h2 className="text-2xl lg:text-3xl font-semibold">Let&apos;s work together.</h2>
+                <p className="text-csFadedWhite mt-2 mb-7 text-sm">
+                    If you would like to discuss my portfolio further or discuss potential
+                    employment, please complete the form below.
+                </p>
+            </article>
+            <section className="mt-10 sm:mt-14 md:mt-20">
+                {formSuccess ? (
+                    <h2 className="w-full text-center">Your message has been sent.</h2>
+                ) : formError.bool ? (
+                    `${formError.message}`
+                ) : (
+                    <form id="form" className="w-full" onSubmit={handleSubmit(processForm)}>
+                        <fieldset className="w-full flex flex-col sm:flex-row justify-between items-center gap-x-2 mb-5">
+                            <fieldset>
+                                <label htmlFor="name">Full name</label>
+                                <input
+                                    id="name"
+                                    placeholder={
+                                        errors?.user_name
+                                            ? `${errors?.user_name?.message}`
+                                            : "Enter your name"
+                                    }
+                                    className={`${
+                                        errors.user_name
+                                            ? "placeholder:text-csRed"
+                                            : "placeholder:text-csFadedWhite"
+                                    }`}
+                                    autoComplete="true"
+                                    {...register("user_name")}
+                                />
+                            </fieldset>
+
+                            <fieldset>
+                                <label htmlFor="email">Full email</label>
+                                <input
+                                    id="email"
+                                    placeholder={
+                                        errors?.user_email
+                                            ? `${errors?.user_email?.message}`
+                                            : "Enter your email"
+                                    }
+                                    className={errors.user_email && "placeholder:text-csRed"}
+                                    autoComplete="true"
+                                    {...register("user_email")}
+                                />
+                            </fieldset>
+
+                            <fieldset>
+                                <label htmlFor="company">Company Name</label>
+                                <input
+                                    id="company"
+                                    placeholder={
+                                        errors?.user_company
+                                            ? `${errors?.user_company?.message}`
+                                            : "Who do you work for?"
+                                    }
+                                    className={errors.user_company && "placeholder:text-csRed"}
+                                    {...register("user_company")}
+                                />
+                            </fieldset>
+                        </fieldset>
+
+                        <fieldset>
+                            <label htmlFor="message">Message</label>
+                            <textarea
+                                id="message"
+                                placeholder={
+                                    errors?.message
+                                        ? `${errors?.message?.message}`
+                                        : "What would you like to say?"
+                                }
+                                className={errors.message && "placeholder:text-csRed"}
+                                {...register("message")}
+                            />
+                        </fieldset>
+
+                        <button
+                            className="text-sm mt-10 py-2 px-4 w-fit bg-white text-csBlack rounded-full border-2 border-csWhite hover:bg-csBlack hover:text-csWhite transition-colors"
+                            type="submit"
+                        >
+                            Send message
+                        </button>
+                    </form>
+                )}
+            </section>
         </section>
     );
 }
