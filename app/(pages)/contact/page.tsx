6e43b07d96aa404fbf6f8afbb0929c7e9cfc4daf
David Lee <Dai180293@gmail.com>
2024-08-08 04:51:30 +0100

    increased header font size

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -3,8 +3,8 @@ export default function page() {
     return (
         <section>
             <article className="max-w-lg mx-auto text-center">
-                <h2 className="text-2xl lg:text-3xl font-semibold">Let&apos;s work together.</h2>
-                <p className="text-csFadedWhite mt-2 mb-7 text-sm">
+                <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold">Let&apos;s work together.</h2>
+                <p className="text-csFadedWhite mt-3 mb-7 text-sm">
                     If you would like to discuss my portfolio further or discuss potential
                     employment, please complete the form below.
                 </p>
